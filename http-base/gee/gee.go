bce252d91c1b4e814a66418d304a1c327c8d6fe0
maxing <1501901576@qq.com>
2021-12-25 22:46:24 +0800

    feat: 路由分组

--- http-base/gee/gee.go
+++ http-base/gee/gee.go
@@ -1,39 +1,68 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandleFunc func(*Context)
 
-type Engine struct {
-	router *router
-}
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandleFunc
+		parent      *RouterGroup
+		engine      *Engine
+	}
+
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup
+	}
+)
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	context := newContext(w, r)
 	engine.router.handle(context)
 }
 
-func (engine *Engine) GET(pattern string, handleFunc HandleFunc) {
-	engine.addRoute("GET", pattern, handleFunc)
+func (group *RouterGroup) GET(pattern string, handleFunc HandleFunc) {
+	group.addRoute("GET", pattern, handleFunc)
 }
 
-func (engine *Engine) POST(pattern string, handleFunc HandleFunc) {
-	engine.addRoute("POST", pattern, handleFunc)
+func (group *RouterGroup) POST(pattern string, handleFunc HandleFunc) {
+	group.addRoute("POST", pattern, handleFunc)
 }
 
-// 这里实现了代码复用
-func (engine *Engine) addRoute(method string, pattern string, handleFunc HandleFunc) {
-	engine.router.addRoute(method, pattern, handleFunc)
+func (group *RouterGroup) addRoute(method string, comp string, handleFunc HandleFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handleFunc)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
 func New() *Engine {
-	return &Engine{
+	engine := &Engine{
 		router: newRouter(),
 	}
+	engine.RouterGroup = &RouterGroup{
+		engine: engine,
+	}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
--- http-base/main.go
+++ http-base/main.go
@@ -7,21 +7,34 @@ import (
 
 func main() {
 	server := gee.New()
-	server.GET("/", func(c *gee.Context) {
+	server.GET("/index", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	server.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := server.Group("v1")
+	{
+		v1.GET("/", func(context *gee.Context) {
+			context.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	server.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v2 := server.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	server.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+	}
+
 	server.Run(":8080")
 }
