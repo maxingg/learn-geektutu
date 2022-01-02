0e68dd2f1ab77aac4067e3c0a10f1811ea156027
maxing <1501901576@qq.com>
2022-01-03 00:09:32 +0800

    feat: 连接SQLite封装

---
+++ gee-orm/geeorm/log/log_test.go
@@ -0,0 +1,13 @@
+package log
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSetLevel(t *testing.T) {
+	SetLevel(ErrorLevel)
+	if infoLog.Writer() == os.Stdout || errorLog.Writer() != os.Stdout {
+		t.Fatal("failed to set log level")
+	}
+}
---
+++ gee-orm/go.mod
@@ -0,0 +1,6 @@
+module geektutu/gee-orm
+
+go 1.17
+
+require geeorm v0.0.0
+replace geeorm => ./geeorm
